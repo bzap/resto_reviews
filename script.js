9d084fb6dbd4851690ab1ad0ee0460792cab5aaf
Linas Aleknevicius <linasaleknevicius@Linass-Air.lan>
2021-11-02 06:07:09 -0400

    add geolocation live map and beginning of form validation

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-
 let map, infoWindow;
+storage = window.sessionStorage;
+
+
+
+function submissionLoc() { 
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            };
+            document.getElementById('sub-lat').value=pos.lat;
+            document.getElementById('sub-long').value=pos.lng;
+            storage.setItem('lat', pos.lat);
+            storage.setItem('lng', pos.lng);
+            
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+
+}
+
 
 function initMap() {
     infoWindow = new google.maps.InfoWindow();
@@ -22,6 +50,8 @@ function initMap() {
             map.setCenter(pos);
             map.setZoom(13);
             infoWindow.close();
+
+
 
             const loc_one = new google.maps.Marker({
         
@@ -60,6 +90,8 @@ function initMap() {
                 map.panTo({lat: pos.lat+0.009, lng: pos.lng+0.004 });
                 infoWindow.setContent('Bento Sushi: <a href="individual_sample.html">' +
                 "Read more</a> ");
+                storage.setItem('lat', pos.lat+0.009);
+                storage.setItem('lng', pos.lng+0.004);
                 infoWindow.open(map, loc_one);
             });
 
@@ -69,7 +101,15 @@ function initMap() {
                 map.panTo({lat: pos.lat+0.014, lng: pos.lng+0.010 });
                 infoWindow.setContent('Umi Sushi Express: <a href="individual_sample.html">' +
                 "Read more</a> ");
+                console.log(pos.lat);
+                storage.setItem('lat', pos.lat+0.014);
+                storage.setItem('lng', pos.lng+0.010);
                 infoWindow.open(map, loc_two);
+                /*
+                if (document.url == 'sample.html'){
+                    initMapIndividual(pos); 
+                }
+                */
             });
 
             google.maps.event.addListener(loc_three, 'click', function(){
@@ -78,6 +118,8 @@ function initMap() {
                 map.panTo({lat: pos.lat+0.003, lng: pos.lng+0.006 });
                 infoWindow.setContent('Sushi On Fennell: <a href="individual_sample.html">' +
                 "Read more</a> ");
+                storage.setItem('lat', pos.lat+0.003);
+                storage.setItem('lng', pos.lng+0.006);
                 infoWindow.open(map, loc_three);
             });
           },
@@ -89,65 +131,70 @@ function initMap() {
         // Browser doesn't support Geolocation
         handleLocationError(false, infoWindow, map.getCenter());
       }
-
-
-    
   }
 
+ function initMapIndividual() { 
+    let lat = parseFloat(storage.getItem('lat'));
+    let lng = parseFloat(storage.getItem('lng'));
+    console.log(lng);
+    console.log(isNaN(lng));
 
+    if (isNaN(lng) || isNaN(lat)){ 
+        console.log("i got here");
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+              (position) => {
+                const pos = {
+                  lat: position.coords.latitude,
+                  lng: position.coords.longitude,
+                };
+                document.getElementById('sub-lat').value=pos.lat;
+                document.getElementById('sub-long').value=pos.lng;
+                storage.setItem('lat', pos.lat);
+                storage.setItem('lng', pos.lng);
+                
+            },
+            () => {
+            
+            }
+          );
+        } else {
+          // Browser doesn't support Geolocation
+        }
+        console.log(parseFloat(storage.getItem('lat')));
 
-/*
-function initMap() {
+    }
+    let lat_new = parseFloat(storage.getItem('lat'));
+    let lng_new = parseFloat(storage.getItem('lng'));
+    console.log(lng_new);
+    //console.log(Object.prototype.toString.call(lat));
+    //console.log(lat);
     map = new google.maps.Map(document.getElementById("map"), {
-      center: { lat: -34.397, lng: 150.644 },
-      zoom: 13,
-    });
-    infoWindow = new google.maps.InfoWindow();
-  
-    const locationButton = document.createElement("button");
-  
-    locationButton.textContent = "Pan to Current Location";
-    locationButton.classList.add("custom-map-control-button");
-    map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-    locationButton.addEventListener("click", () => {
-      // Try HTML5 geolocation.
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            const pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude,
-            };
-  
-            infoWindow.setPosition(pos);
-            infoWindow.setContent("Location found.");
-            infoWindow.open(map);
-            map.setCenter(pos);
-          },
-          () => {
-            handleLocationError(true, infoWindow, map.getCenter());
-          }
-        );
-      } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-      }
-    });
-  }
-  */
+        center: { lat: lat, lng: lng },
+        zoom: 2,
+      }); 
+      map.setCenter({ lat: lat, lng: lng });
+      map.setZoom(14);
+
+
+      const loc_one = new google.maps.Marker({
+        position: { lat: lat, lng: lng },
+        label: 'A',
+        map,  
+      }); 
+ }
+
+
   function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
     infoWindow.setContent(
       browserHasGeolocation
-        ? "Error: The Geolocation service failed."
+        ? "Please allow Geolocation permissions."
         : "Error: Your browser doesn't support geolocation."
     );
     infoWindow.open(map);
   }
 
-// https://developers.google.com/maps/documentation/javascript/examples/map-geolocation 
-
-// use this to determine your current geolocation on the search press 
 
 
 
